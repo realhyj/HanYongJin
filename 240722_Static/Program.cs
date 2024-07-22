20e08f8b1369d7f4acfe5d9f4835ee14c27451f9
realhyj <9k9k9k9k@naver.com>
2024-07-22 16:17:08 +0900

    Split by subject & Static Study ADD
    파일이 무슨 공부를 했는지 대강 알 수 있도록 파일 명을 수정.
    (240722 -> 240722_Class)
    
    Static을 활용하여 계산기를 만든 것을  추가

---
+++ 240722_Static/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Static
+{
+    internal class Program
+    {
+        public static class MyCalculator
+        {
+            public static double Add(double x, double y)
+            {
+                return x + y; 
+            }
+            public static double Subtract(double x, double y) 
+            {
+                return x - y; 
+            }
+            public static double Multiply(double x, double y)
+            {
+                return x * y;
+            }
+            public static double Divide(double x, double y)
+            {
+                if (y == 0)
+                {
+                    Console.WriteLine("Error!!");
+                    return double.NaN;
+                }
+                else
+                {
+                    return x / y;
+                }
+            }
+            public static double Squared(double x, double y)
+            {
+                return Math.Pow(x, y);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("계산기를 이용할 첫 번째 수를 입력해주세요.");
+            double.TryParse(Console.ReadLine(), out double num1);
+            Console.WriteLine("두 번째 수를 입력해주세요.");
+            double.TryParse(Console.ReadLine(), out double num2);
+
+            Console.WriteLine($"{num1} + {num2} = {MyCalculator.Add(num1, num2)}");
+            Console.WriteLine($"{num1} - {num2} = {MyCalculator.Subtract(num1, num2)}");
+            Console.WriteLine($"{num1} * {num2} = {MyCalculator.Multiply(num1, num2)}");
+            Console.WriteLine($"{num1} / {num2} = {MyCalculator.Divide(num1, num2)}");
+            Console.WriteLine($"{num1} ^ {num2} = {MyCalculator.Squared(num1, num2)}");
+        }
+    }
+}
